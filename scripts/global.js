a43598e1343ec0968ae34c410319e0f173bb95c0
azsheishenov <a.sheyshenov@bk>
2025-02-24 19:15:39 +0600

    added new navigation menu

--- scripts/global.js
+++ scripts/global.js
@@ -9,3 +9,17 @@ export const checkUserAuth = () => {
         window.location.replace('../pages/login.html');
     }
 }
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.getElementById('menu')
+    const burgerActive = document.getElementById('menu-active')
+
+    burger.addEventListener('click', () => {
+        burger.classList.add('active')
+    })
+
+    burgerActive.addEventListener('click', () => {
+        burger.classList.remove('active')
+    })
+})
